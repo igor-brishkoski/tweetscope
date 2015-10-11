3a8415c608ff078eb1008433494aa22ccd3ca344
igor-brishkoski <igor.brishkoski@gmail.com>
2015-10-11 16:39:02 -0400

    don;t create new ones

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -10,20 +10,23 @@ class Keyword < ActiveRecord::Base
     end
 
     client.search(self.word, result_type: "recent").take(10).collect do |tweet|
-      new_tweet                  = Tweet.new
+      new_tweet = Tweet.find_by_tweet_id(tweet.id.to_s)
+      if new_tweet.nil?
+        new_tweet                  = Tweet.new
 
-      new_tweet.tweet_id         = tweet.id.to_s
-      new_tweet.tweet_created_at = tweet.created_at
-      new_tweet.text             = tweet.text
+        new_tweet.tweet_id         = tweet.id.to_s
+        new_tweet.tweet_created_at = tweet.created_at
+        new_tweet.text             = tweet.text
 
-      new_tweet.user_uid         = tweet.user.id
-      new_tweet.user_name        = tweet.user.name
-      new_tweet.user_screen_name = tweet.user.screen_name
-      new_tweet.user_image_url   = tweet.user.profile_image_url
+        new_tweet.user_uid         = tweet.user.id
+        new_tweet.user_name        = tweet.user.name
+        new_tweet.user_screen_name = tweet.user.screen_name
+        new_tweet.user_image_url   = tweet.user.profile_image_url
 
-      new_tweet.keyword          = self
+        new_tweet.keyword          = self
 
-      new_tweet.save
+        new_tweet.save
+      end
     end
   end
 end
