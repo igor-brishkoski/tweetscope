738e6da89be73ce9adda74f10524fd50c44c201d
igor-brishkoski <igor.brishkoski@gmail.com>
2015-10-11 15:49:42 -0400

    twitter cliend and figaro

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+gem 'twitter'
 
 gem 'rails_12factor', group: :production 
 
@@ -38,6 +39,7 @@ gem 'rails_12factor', group: :production
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  gem 'figaro'
 end
 
 group :development do
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # client = Twitter::REST::Client.new do |config|
+  #   config.consumer_key        = "gWOkdIMtQ1RJ8Q6h87PIx6536"
+  #   config.consumer_secret     = "fcWxuXmsu4vmTn757uCCnZzXBNnLdIFjNlo4yN7YdQXOZrO1m6"
+  #   config.access_token        = "927601872-m1ep1zgphFJN6AeByxWAAcNp90IRDG8KS8SkBWhJ"
+  #   config.access_token_secret = "QmfZg2z0nkB5Xle43qhnU72W2WgpNfeXXporQOJnQSnSk"
+  # end
 end
